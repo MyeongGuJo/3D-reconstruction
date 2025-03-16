dbde865ca1810f1d5b5df9dc052c05ab8b0e93ef
MyeongGuJo <whaudrn3708@hanyang.ac.kr>
2025-03-16 15:01:08 +0900

    nerf data preprocessing

--- get-nerf-data.py
+++ get-nerf-data.py
@@ -10,16 +10,16 @@ output_path = 'coffee_data.npz'
 if __name__=="__main__":
     print("Get poses...")
     image_files, poses = utils.nerf_data_format.get_poses(reconstruction_path)
-    print(poses.shape)
+    print(poses.shape, type(poses))
 
     print("Get images...")
     images = utils.nerf_data_format.get_images(image_files)
-    print(images.shape)
+    print(images.shape, type(images))
     
     H = images.shape[1]
     W = images.shape[2]
     focal = 1.2 * max(H, W)
-    print(f"focal length: {focal}")
+    print(f"focal length: {focal}, {type(focal)}")
 
     np.savez(output_path, images=images, poses=poses, focal=focal)
     print(f"Data saved to {output_path}")
